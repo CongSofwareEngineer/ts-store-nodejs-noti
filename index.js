4ea4554c49bf9926a27d7b7b9e4d2a52fc3cc693
Cong_Bacoor <cong@bacoor.co>
2024-12-12 13:46:59 +0700

    fix noti

--- index.js
+++ index.js
@@ -1,58 +1,101 @@
-const admin = require("firebase-admin");
-const express = require("express");
-const cors = require("cors");
+const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(cors());
-const serviceAccount = require("./firebase-config.json");
+const serviceAccount = require('./firebase-config.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-app.get("/ping", async(req, res) => {
+app.get('/ping', async (req, res) => {
   res.send({
-    isWork:true
-  })
-})
+    isWork: true,
+  });
+});
 
-app.post("/noti/push", async(req, res) => {
-   
-  const listToken = req.body.listToken;
-  const messageObj = req.body.message;
-  const messageArr=listToken.map(token=>({
-    ...messageObj,
-    token
-  }))
-  // const message = {
-  //   notification: {
-  //     title: messageObj.title,
-  //     body: messageObj.body||{},
-  //   },
-  //   token: listToken,
-  //   data: {
-  //     id:messageObj.data.id,
-  //     name: messageObj.data.name,
-  //   },
-  // };
- await admin
+app.get('/push/test', async (req, res) => {
+  const bodyTest = {
+    message: {
+      notification: {
+        title: 'messageObj',
+        body: 'messageObj',
+      },
+      data: {
+        id: '1',
+        name: 'test',
+      },
+    },
+    listToken: ['c_pSI8AgsrbgRWSlojMfPT:APA91bGfg9UOfNeKWYxfGZPivR3aK0EGeFR-tqtxOXjbwAP1ohK9w4H2xkZP9vdeoXLjfLGVm03oXe6Tw95yvfNm7kFGVYp-zP_BzDbnBsZujSAFrQX4q2o'],
+  };
+
+  const messageArr = bodyTest.listToken.map((token) => ({
+    ...bodyTest.message,
+    token,
+  }));
+  console.log({ messageArr });
+
+  const message = {
+    notification: {
+      title: 'Hello World',
+      body: ' req.body',
+    },
+    token: 'c_pSI8AgsrbgRWSlojMfPT:APA91bGfg9UOfNeKWYxfGZPivR3aK0EGeFR-tqtxOXjbwAP1ohK9w4H2xkZP9vdeoXLjfLGVm03oXe6Tw95yvfNm7kFGVYp-zP_BzDbnBsZujSAFrQX4q2o',
+    data: {
+      id: '1',
+      name: 'test',
+    },
+  };
+
+  await admin
     .messaging()
     .sendEach(messageArr)
     .then((result) => {
-      console.log("====================================");
+      console.log('====================================');
       console.log({ result });
-      console.log("====================================");
-      res.sendStatus(200).send({
-        success:true, 
+      console.log('====================================');
+      res.send({
+        success: true,
       });
     })
     .catch((err) => {
-      console.log({err});
-      res.sendStatus(400).send({
-        err
+      console.log({ err });
+      res.send({
+        err,
       });
     });
 });
+ 
 
-app.listen( process.env.PORT ||3002 , () => {
-  // app.listen(3002, () => {
-    console.log("listening on port", process.env.PORT || 3002);
+app.get('/noti/push', async (req, res) => {
+ try {
+  const queryString = req.query?.query;
+  if (typeof queryString === 'string') {
+    const data = JSON.parse(queryString);
+    const listToken = data.listToken  ;
+    const messageObj = data.message ;
+    const messageArr = listToken.map((token) => ({
+      ...messageObj,
+      token,
+    })); 
+    await admin.messaging().sendEach(messageArr)
+  } else {
+    res.status(400).send({
+      error: true,
+    });
+  }
+
+  res.send({
+    success: true,
   });
+ } catch (error) {
+  res.status(400).send({
+    error: true,
+  });
+ }
+});
+
+app.listen(process.env.PORT || 3005, () => {
+  // app.listen(3002, () => {
+  console.log('listening on port', process.env.PORT || 3005);
+});
